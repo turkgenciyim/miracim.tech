3362f70991fb65cecd0f7ed1fcf1ac0332e06d94
Ahmet Miraç Bakır <ahmetmiracbakir@protonmail.com>
2023-07-01 01:04:21 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
+const { screens } = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        xs: "425px",
+        ...screens,
       },
     },
   },
   plugins: [],
-}
+};
