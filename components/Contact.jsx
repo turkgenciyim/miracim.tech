69e016d72599faf9c66e6580448942eab73253eb
Miraç <ahmetmiracbakir@protonmail.com>
2023-12-28 15:40:53 +0300

    init

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,25 +1,34 @@
 import React from 'react'
 import { BiLogoDiscordAlt } from 'react-icons/bi'
 import Link from 'next/link'
+import Image from 'next/image'
 const Showcase = () => {
   return (
-    <div
-      id='iletisim'
-      className='flex flex-col items-center justify-center px-4 space-y-8 bg-no-repeat bg-cover bg-[url("/discord-pattern.png")] bg-right-top font-bold py-14'
-    >
-      <h2 className='px-4 text-3xl font-bold tracking-tighter text-center xs:text-4xl text-slate-100 md:text-5xl lg:text-6xl'>
-        Discord üzerinden istersen <br /> sohbet, istersen iş konuşalım.
-      </h2>
-      <Link
-        href={'https://discord.com/users/619925060387340299'}
-        className='flex items-center justify-between gap-8 py-4 pl-10 pr-6 text-lg transition-all shadow bg-blue-500/20 backdrop-blur-sm active:ring active:ring-primary-900 active:scale-95 md:py-5 md:pr-8 hover:bg-primary-400/50 hover:backdrop-blur-none group md:pl-14 rounded-xl'
+    <section className='relative z-10'>
+      <Image
+        src={'/discord-pattern.png'}
+        fill
+        alt='Contact'
+        className='object-cover object-center pointer-events-none -z-10'
+      />
+      <div
+        id='iletisim'
+        className='z-10 flex flex-col items-center justify-center px-4 space-y-8 font-bold py-14'
       >
-        <span className='inline-block font-bold tracking-tight text-white uppercase transition-all group-hover:text-white'>
-          İletişime Geç
-        </span>
-        <BiLogoDiscordAlt className='w-10 h-10 transition-all duration-300 fill-primary-500  group-hover:rotate-[360deg] group-hover:fill-white' />
-      </Link>
-    </div>
+        <h2 className='px-4 text-3xl font-bold tracking-tighter text-center xs:text-4xl text-slate-100 md:text-5xl lg:text-6xl'>
+          Discord üzerinden istersen <br /> sohbet, istersen iş konuşalım.
+        </h2>
+        <Link
+          href={'https://discord.com/users/619925060387340299'}
+          className='flex items-center justify-between gap-8 py-4 pl-10 pr-6 text-lg transition-all shadow bg-blue-500/20 backdrop-blur-sm active:ring active:ring-primary-900 active:scale-95 md:py-5 md:pr-8 hover:bg-primary-400/50 hover:backdrop-blur-none group md:pl-14 rounded-xl'
+        >
+          <span className='inline-block font-bold tracking-tight text-white uppercase transition-all group-hover:text-white'>
+            İletişime Geç
+          </span>
+          <BiLogoDiscordAlt className='w-10 h-10 transition-all duration-300 fill-primary-500  group-hover:rotate-[360deg] group-hover:fill-white' />
+        </Link>
+      </div>
+    </section>
   )
 }
 
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import { motion } from 'framer-motion'
 import {
   SiAdobephotoshop,
@@ -18,7 +19,6 @@ import {
   SiTailwindcss,
   SiVercel
 } from 'react-icons/si'
-import { CiNoWaitingSign } from 'react-icons/ci'
 const Skills = () => {
   const introHeaderVariants = {
     hide: {
@@ -68,96 +68,106 @@ const Skills = () => {
     }
   }
   return (
-    <div className='relative flex flex-col items-center justify-center bg-no-repeat bg-cover bg-[url("/skill-pattern.png")] gap-12 pt-12 px-12 md:pb-12 m-2.5 bg-bottom rounded-xl'>
-      <motion.h2
-        initial='hide'
-        id='becerilerim'
-        whileInView='show'
-        exit='hide'
-        variants={introHeaderVariants}
-        className='max-w-5xl mx-auto font-sans text-4xl font-bold tracking-tighter text-center text-white select-none xs:text-5xl md:text-8xl'
-      >
-        Web sitelerim için kullandığım teknolojiler
-      </motion.h2>
-      <motion.ul
-        animate='visible'
-        initial='hidden'
-        whileInView={'show'}
-        exit='hide'
-        variants={introHeaderIconsVariants}
-        className='grid m-0 overflow-hidden list-none grid-cols-[repeat(3,_1fr)] sm:grid-cols-[repeat(4,_1fr)] md:grid-cols-[repeat(5,_1fr)] gap-2 p-8 md:p-12 pb-3 md:pb-12 rounded-xl'
-      >
-        <motion.li variants={introHeaderIcons} className='relative z-10'>
-          <div className='absolute hidden h-10 px-3 py-1 font-bold text-white translate-x-1/2 -translate-y-1/2 -z-10 sm:block bg-gradient-to-tr from-primary-500 to-cyan-600 -top-4 right-1/2 rounded-t-2xl'>
-            NextJS
-          </div>
-          <SiNextdotjs className='z-20 w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-gradient-to-tr from-sky-500 to-cyan-600 rounded-2xl fill-slate-200 hover:fill-white' />
-        </motion.li>
-        <motion.li variants={introHeaderIcons} className='relative'>
-          <div className='absolute z-10 hidden h-10 px-3 py-1 font-bold translate-x-1/2 -translate-y-1/2 text-primary-400 md:block bg-primary-800/5 -top-4 right-1/2 rounded-t-2xl drop-shadow-xl'>
-            ReactJS
-          </div>
-          <SiReact className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-cyan-500' />
-        </motion.li>
-        <motion.li variants={introHeaderIcons} className='relative'>
-          <div className='absolute z-10 hidden h-10 px-3 py-1 font-bold translate-x-1/2 -translate-y-1/2 md:block text-emerald-500 bg-sky-800/5 -top-4 right-1/2 rounded-t-2xl drop-shadow-xl'>
-            MongoDB
-          </div>
-          <SiMongodb className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-emerald-500' />
-        </motion.li>
-        <motion.li variants={introHeaderIcons} className='relative'>
-          <div className='absolute z-10 hidden h-10 px-3 py-1 font-bold translate-x-1/2 -translate-y-1/2 md:block text-cyan-500 bg-sky-800/5 -top-4 right-1/2 rounded-t-2xl drop-shadow-xl'>
-            TailwindCSS
-          </div>
-          <SiTailwindcss className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-cyan-500' />
-        </motion.li>
-        <motion.li variants={introHeaderIcons} className='relative'>
-          <div className='absolute z-10 hidden h-10 px-3 py-1 font-bold text-white translate-x-1/2 -translate-y-1/2 md:block bg-slate-800/5 -top-4 right-1/2 rounded-t-2xl drop-shadow-xl'>
-            Framer
-          </div>
-          <SiFramer className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-slate-900' />
-        </motion.li>
+    <section className='relative'>
+      <Image
+        src={'/skill-pattern.png'}
+        fill
+        alt='Skills'
+        sizes='10vw, 10vw'
 
-        <motion.li variants={introHeaderIcons}>
-          <SiExpress className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-amber-500' />
-        </motion.li>
-        <motion.li variants={introHeaderIcons}>
-          <SiNodedotjs className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-emerald-600' />
-        </motion.li>
-        <motion.li variants={introHeaderIcons}>
-          <SiAmazonaws className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-orange-500' />
-        </motion.li>
-        <motion.li variants={introHeaderIcons}>
-          <SiSupabase className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-emerald-600' />
-        </motion.li>
-        <motion.li variants={introHeaderIcons}>
-          <SiPlanetscale className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-current' />
-        </motion.li>
-        <motion.li variants={introHeaderIcons}>
-          <SiPrisma className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-primary-500' />
-        </motion.li>
-        <motion.li variants={introHeaderIcons}>
-          <SiVercel className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-current' />
-        </motion.li>
-        <motion.li variants={introHeaderIcons}>
-          <SiAdobephotoshop className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-primary-600' />
-        </motion.li>
-        <motion.li variants={introHeaderIcons}>
-          <SiJavascript className='w-16 h-16 p-3 transition-all duration-300 shadow cursor-not-allowed xs:p-4 xs:w-28 xs:h-28 fill-amber-500 bg-amber-600/20 rounded-xl' />
-        </motion.li>
-        <motion.li variants={introHeaderIcons}>
-          <SiCss3 className='w-16 h-16 p-3 transition-all duration-300 shadow cursor-not-allowed xs:p-4 xs:w-28 xs:h-28 fill-primary-500 bg-primary-600/20 rounded-xl' />
-        </motion.li>
-        <motion.li variants={introHeaderIcons}>
-          <SiHtml5 className='w-16 h-16 p-3 transition-all duration-300 shadow cursor-not-allowed xs:p-4 xs:w-28 xs:h-28 fill-orange-500 bg-orange-600/20 rounded-xl' />
-        </motion.li>
-        <motion.li variants={introHeaderIcons} className='w-full col-span-4'>
-          <h1 className='flex items-center justify-center w-full h-full p-3 text-xl font-medium text-center text-gray-100 transition-all duration-300 shadow cursor-not-allowed sm:text-2xl  xs:p-4 hover:bg-white/10 bg-white/5 backdrop-blur-[2px] rounded-xl'>
-            Daha fazla eklemek <br /> için çabalıyorum.
-          </h1>
-        </motion.li>
-      </motion.ul>
-    </div>
+        className='object-cover object-center pointer-events-none'
+      />
+      <div className='z-10 relative flex flex-col items-center justify-center bg-no-repeat bg-cover bg-[url("/skill-pattern.png")] gap-12 pt-12 px-12 md:pb-12 m-2.5 bg-bottom rounded-xl'>
+        <motion.h2
+          initial='hide'
+          id='becerilerim'
+          whileInView='show'
+          exit='hide'
+          variants={introHeaderVariants}
+          className='max-w-5xl mx-auto font-sans text-4xl font-bold tracking-tighter text-center text-white select-none xs:text-5xl md:text-8xl'
+        >
+          Web sitelerim için kullandığım teknolojiler
+        </motion.h2>
+        <motion.ul
+          animate='visible'
+          initial='hidden'
+          whileInView={'show'}
+          exit='hide'
+          variants={introHeaderIconsVariants}
+          className='grid m-0 overflow-hidden list-none grid-cols-[repeat(3,_1fr)] sm:grid-cols-[repeat(4,_1fr)] md:grid-cols-[repeat(5,_1fr)] gap-2 p-8 md:p-12 pb-3 md:pb-12 rounded-xl'
+        >
+          <motion.li variants={introHeaderIcons} className='relative z-10'>
+            <div className='absolute hidden h-10 px-3 py-1 font-bold text-white translate-x-1/2 -translate-y-1/2 -z-10 sm:block bg-gradient-to-tr from-primary-500 to-cyan-600 -top-4 right-1/2 rounded-t-2xl'>
+              NextJS
+            </div>
+            <SiNextdotjs className='z-20 w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-gradient-to-tr from-sky-500 to-cyan-600 rounded-2xl fill-slate-200 hover:fill-white' />
+          </motion.li>
+          <motion.li variants={introHeaderIcons} className='relative'>
+            <div className='absolute z-10 hidden h-10 px-3 py-1 font-bold translate-x-1/2 -translate-y-1/2 text-primary-400 md:block bg-primary-800/5 -top-4 right-1/2 rounded-t-2xl drop-shadow-xl'>
+              ReactJS
+            </div>
+            <SiReact className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-cyan-500' />
+          </motion.li>
+          <motion.li variants={introHeaderIcons} className='relative'>
+            <div className='absolute z-10 hidden h-10 px-3 py-1 font-bold translate-x-1/2 -translate-y-1/2 md:block text-emerald-500 bg-sky-800/5 -top-4 right-1/2 rounded-t-2xl drop-shadow-xl'>
+              MongoDB
+            </div>
+            <SiMongodb className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-emerald-500' />
+          </motion.li>
+          <motion.li variants={introHeaderIcons} className='relative'>
+            <div className='absolute z-10 hidden h-10 px-3 py-1 font-bold translate-x-1/2 -translate-y-1/2 md:block text-cyan-500 bg-sky-800/5 -top-4 right-1/2 rounded-t-2xl drop-shadow-xl'>
+              TailwindCSS
+            </div>
+            <SiTailwindcss className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-cyan-500' />
+          </motion.li>
+          <motion.li variants={introHeaderIcons} className='relative'>
+            <div className='absolute z-10 hidden h-10 px-3 py-1 font-bold text-white translate-x-1/2 -translate-y-1/2 md:block bg-slate-800/5 -top-4 right-1/2 rounded-t-2xl drop-shadow-xl'>
+              Framer
+            </div>
+            <SiFramer className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-slate-900' />
+          </motion.li>
+
+          <motion.li variants={introHeaderIcons}>
+            <SiExpress className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-amber-500' />
+          </motion.li>
+          <motion.li variants={introHeaderIcons}>
+            <SiNodedotjs className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-emerald-600' />
+          </motion.li>
+          <motion.li variants={introHeaderIcons}>
+            <SiAmazonaws className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-orange-500' />
+          </motion.li>
+          <motion.li variants={introHeaderIcons}>
+            <SiSupabase className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-emerald-600' />
+          </motion.li>
+          <motion.li variants={introHeaderIcons}>
+            <SiPlanetscale className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-current' />
+          </motion.li>
+          <motion.li variants={introHeaderIcons}>
+            <SiPrisma className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-primary-500' />
+          </motion.li>
+          <motion.li variants={introHeaderIcons}>
+            <SiVercel className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-current' />
+          </motion.li>
+          <motion.li variants={introHeaderIcons}>
+            <SiAdobephotoshop className='w-16 h-16 p-3 transition-all duration-200 shadow cursor-pointer select-none xs:p-4 xs:w-28 xs:h-28 active:scale-95 hover:scale-105 bg-slate-50/5 hover:bg-white/100 rounded-2xl fill-white hover:fill-primary-600' />
+          </motion.li>
+          <motion.li variants={introHeaderIcons}>
+            <SiJavascript className='w-16 h-16 p-3 transition-all duration-300 shadow cursor-not-allowed xs:p-4 xs:w-28 xs:h-28 fill-amber-500 bg-amber-600/20 rounded-xl' />
+          </motion.li>
+          <motion.li variants={introHeaderIcons}>
+            <SiCss3 className='w-16 h-16 p-3 transition-all duration-300 shadow cursor-not-allowed xs:p-4 xs:w-28 xs:h-28 fill-primary-500 bg-primary-600/20 rounded-xl' />
+          </motion.li>
+          <motion.li variants={introHeaderIcons}>
+            <SiHtml5 className='w-16 h-16 p-3 transition-all duration-300 shadow cursor-not-allowed xs:p-4 xs:w-28 xs:h-28 fill-orange-500 bg-orange-600/20 rounded-xl' />
+          </motion.li>
+          <motion.li variants={introHeaderIcons} className='w-full col-span-4'>
+            <h1 className='flex items-center justify-center w-full h-full p-3 text-xl font-medium text-center text-gray-100 transition-all duration-300 shadow cursor-not-allowed sm:text-2xl  xs:p-4 hover:bg-white/10 bg-white/5 backdrop-blur-[2px] rounded-xl'>
+              Daha fazla eklemek <br /> için çabalıyorum.
+            </h1>
+          </motion.li>
+        </motion.ul>
+      </div>
+    </section>
   )
 }
 
